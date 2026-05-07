031e408fc07f314979f5820f08276042c27748ab
naite-101 <2236980615@qq.com>
2026-05-07 21:43:12 +0800

    把代码的架构从“gcc”架构更改成“Cmake”架构

---
+++ build/CMakeFiles/videoplayer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/videoplayer.dir/videoplayer.c.o"
+  "videoplayer"
+  "videoplayer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/videoplayer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
